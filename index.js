322a29ecfe3cdd393c8faa91e2e0d427e15b0616
guru <dr.guru750@gmail.com>
2024-04-19 22:53:24 +0530

    first

---
+++ index.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+mongoose.connect('mongodb+srv://drguru750:8858@lakshmigarden.ony18xo.mongodb.net/?retryWrites=true&w=majority&appName=lakshmigarden')
+    .then(() => {
+        console.log("Databases has been connected");
+    })
+    .catch(() => {
+        console.log("Databases not connected");
+    })
+
+const UserSchema = new mongoose.Schema({
+    fname: { type: String },
+    lname: { type: String },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+
+})
+
+const Collection = mongoose.model('yellowblogs', UserSchema)
+app.use(express.json());
+app.use(cors());
+
+app.post('/posting', async (req, resp) => {
+    try {
+        const user = new Collection(req.body);
+        const result = await user.save();
+        const dataSending = result.toObject();
+        resp.send(dataSending);
+    }
+    catch (e) {
+        console.log(e);
+    }
+})
+//login
+app.post('/login', async (req, res) => {
+    const { email, password1 } = req.body;
+    try {
+        const user = await Collection.findOne({ email, password1 });
+        if (user) {
+            res.status(200).json({ success: true, message: 'Login successful' });
+        } else {
+
+            res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        console.error('Error during login:', error);
+        res.status(500).json({ success: false, message: 'An error occurred. Please try again later.' });
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`App is listening to ${port}`);
+})
